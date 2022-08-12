87b271deafcf1e028a4b8007ec9d7bd0b5adc6fa
unknown <deeptismita.xworkz@gamil.com>
2022-08-12 08:29:45 +0530

    message

---
+++ Java File/src/com/xworkz/Example.java
@@ -0,0 +1,14 @@
+package com.xworkz;
+
+public class Example {
+
+	public static void main(String[] args) {
+		
+		for(int index = 0; index < 10; index++) {
+			
+			System.out.println("Deepti is a good girl.");
+		}
+
+	}
+
+}
